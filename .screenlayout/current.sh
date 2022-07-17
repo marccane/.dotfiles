8361bef303771e17d3b1bf96d3a28c93b7cca235
Marc Cane <marc00077@hotmail.com>
2022-07-17 11:41:12 +0200

    files

--- .screenlayout/current.sh
+++ .screenlayout/current.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --off
-/home/markus/.screenlayout/secundari.sh
+#/home/markus/.screenlayout/secundari.sh
