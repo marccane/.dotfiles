bcb9a77d306b4c3bf42800c7f4f573deadbc3bc5
Marc Cané <marc00077@hotmail.com>
2023-12-01 15:21:27 +0100

    Work setup, i3config still needs to be merged. Added .xprofile. Fixed 6 taking too long to be pressed in zshrc, 5 still janky

--- .screenlayout/current.sh
+++ .screenlayout/current.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DVI-D-0 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal
