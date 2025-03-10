6b5c5b621291f0d3b7da9411f53cce94655d6727
Marc Cané <marc00077@hotmail.com>
2025-03-10 12:35:58 +0100

    Guarrada per forçar que xrandr sempre la reaplicació de la config

--- .screenlayout/current.sh
+++ .screenlayout/current.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --off --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal -r "74.97" --output DP-1-3 --primary --mode 1920x1080 --pos 3840x0 --rotate normal -r "74.97"
+c='xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
+toggleFilename="/tmp/screenLayoutForceApplyWorkaround"
+
+if [ -e "$toggleFilename" ]; then
+    echo executant 1
+    rm "$toggleFilename"
+    c2="${c/ 0x0 / 0x1 }" #modificació tonta perquè detecti config diferent i forci l'aplicació d'aquesta
+else
+    echo executant 2
+    touch "$toggleFilename"
+    c2=$c #comanda sense modificar
+fi
+
+$($c2) #executar comanda possiblement modificada
