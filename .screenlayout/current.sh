20269f3c15671f5f4b30c0b2f66cc5f4f65d27dd
Marc Cané <marc00077@hotmail.com>
2026-02-17 12:12:14 +0100

    Pantalla grossa com a principal (per les notis). Fix pel hack de forçar aplicar settings, amb la nova versio xrandr detecta que les configuracions de pantalla incorrectes (quan es solapen framebuffers) i ara canvia un pixel vertical, que és valid i no solapa res

--- .screenlayout/current.sh
+++ .screenlayout/current.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-c='xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
+c='xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --scale 0.6x0.6 --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
 toggleFilename="/tmp/screenLayoutForceApplyWorkaround"
 
 if [ -e "$toggleFilename" ]; then
     echo executant 1
     rm "$toggleFilename"
-    c2="${c/ 0x0 / 0x1 }" #modificació tonta perquè detecti config diferent i forci l'aplicació d'aquesta
+    c2="${c/ 3840x0 / 3840x1 }" #modificació tonta perquè detecti config diferent i forci l'aplicació d'aquesta
 else
     echo executant 2
     touch "$toggleFilename"
