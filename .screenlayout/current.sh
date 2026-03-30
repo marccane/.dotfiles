921ef95271c568f497eef7cb579ed0140857ab5f
Marc Cané <marc00077@hotmail.com>
2026-03-30 17:28:32 +0200

    setup 4 pantalles

--- .screenlayout/current.sh
+++ .screenlayout/current.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-c='xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --scale 1x1 --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
+c='xrandr --output eDP-1 --mode 1920x1080 --pos 5757x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --mode 1920x1080 --pos 3840x0 --rotate normal -r 74.97 --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
 toggleFilename="/tmp/screenLayoutForceApplyWorkaround"
 
 if [ -e "$toggleFilename" ]; then
