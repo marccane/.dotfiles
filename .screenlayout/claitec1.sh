8128072e8f390030d65f11f5990ba17eabed7b0c
Marc Cané <marc00077@hotmail.com>
2026-04-20 11:24:23 +0200

    upgrade dels scripts de setup de pantalles. .current ara és un wrapper script que li pots passar parametre 1 o 2 per triar el setup i claitec1/2 son scripts subordinats que no s'han d'executar manualment

---
+++ .screenlayout/claitec1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+c='xrandr --output eDP-1 --mode 1920x1080 --pos 5757x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --mode 1920x1080 --pos 3840x0 --rotate normal -r 74.97 --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
+toggleFilename="/tmp/screenLayoutForceApplyWorkaround"
+
+if [ -e "$toggleFilename" ]; then
+    echo executant 1
+    rm "$toggleFilename"
+    c2="${c/ 3840x0 / 3840x1 }" #modificació tonta perquè detecti config diferent i forci l'aplicació d'aquesta
+else
+    echo executant 2
+    touch "$toggleFilename"
+    c2=$c #comanda sense modificar
+fi
+
+$($c2) #executar comanda possiblement modificada
--- .screenlayout/current.sh
+++ .screenlayout/current.sh
@@ -1,15 +1,23 @@
-#!/bin/sh
-c='xrandr --output eDP-1 --mode 1920x1080 --pos 5757x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal -r 74.97 --output DP-1-2 --mode 1920x1080 --pos 3840x0 --rotate normal -r 74.97 --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal -r 74.97'
-toggleFilename="/tmp/screenLayoutForceApplyWorkaround"
+#!/bin/bash
 
-if [ -e "$toggleFilename" ]; then
-    echo executant 1
-    rm "$toggleFilename"
-    c2="${c/ 3840x0 / 3840x1 }" #modificació tonta perquè detecti config diferent i forci l'aplicació d'aquesta
-else
-    echo executant 2
-    touch "$toggleFilename"
-    c2=$c #comanda sense modificar
-fi
+# 1=claitec1, 2=claitec2 (default)
+LOCATION=${1:-2}
 
-$($c2) #executar comanda possiblement modificada
+case "$LOCATION" in
+  1)
+    ~/.screenlayout/claitec1.sh
+    # historicament l'script claitec1 ja the el rate integrat i no ho trec de moment
+    ;;
+  2)
+    ~/.screenlayout/claitec2.sh
+    sleep 1
+    xrandr --output DP-1-1 --mode 1920x1080 --rate 74.97 --output DP-1-2 --mode 1920x1080 --rate 100.00
+    #--mode força l'aplicació de la config
+    ;;
+  *)
+    echo "Usage: $0 [1|2]"
+    echo "  1 = claitec1 layout"
+    echo "  2 = claitec2 layout (default)"
+    exit 1
+    ;;
+esac
