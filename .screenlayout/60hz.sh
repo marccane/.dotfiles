ca90d8261acd20a66b7a0f10cb44138d8994c570
Marc Cane <marc00077@hotmail.com>
2026-04-26 21:48:09 +0200

    canviar pantalla secundaria de port DP perquè sembla que s'ha espatllat l'altre...

--- .screenlayout/60hz.sh
+++ .screenlayout/60hz.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 -r 59.95 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-A-0 --off --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x360 --rotate normal --output DisplayPort-1 --primary -r 59.95 --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off --output DVI-D-0 --off
